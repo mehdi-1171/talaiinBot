17ee6df88ad46e2d740ab7530f79ff8b3069fc0e
mehdi <m.habibian1171@gmail.com>
2026-04-29 17:00:24 +0330

    feat: create telegram bot and send data

---
+++ tel_bot.py
@@ -0,0 +1,44 @@
+import asyncio
+import telegram
+from dotenv import load_dotenv
+
+from get_data import GoldPriceFetcher
+
+
+load_dotenv()
+
+
+class GoldTelegramBot:
+    TOKEN = os.getenv("TOKEN")
+    CHAT_ID = os.getenv("CHAT_ID")
+
+    def __init__(self):
+        self.bot = telegram.Bot(token=self.TOKEN)
+        self.chat_id = self.CHAT_ID
+        self.fetcher = GoldPriceFetcher()
+        self.data = []
+
+    def _format_message(self):
+        msg = ""
+        for item in self.data:
+            msg += f"📊 *{item['title']}*\n"
+            msg += f"💰 قیمت خرید: `{item['buy']}` تومان\n"
+            msg += f"💸 قیمت فروش: `{item['sell']}` تومان\n"
+            msg += f"📈 بیشترین امروز: `{item['max_buy']}` تومان\n"
+            msg += f"📉 کمترین امروز: `{item['min_buy']}` تومان\n"
+            msg += f"🕐 آخرین بروزرسانی: {item['updated_at']}\n"
+        return msg
+
+    async def send(self):
+        self.data = self.fetcher.get()
+        message = self._format_message()
+        await self.bot.send_message(
+            chat_id=self.chat_id,
+            text=message,
+            parse_mode="Markdown"
+        )
+        print("پیام ارسال شد ✅")
+
+
+bot = GoldTelegramBot()
+asyncio.run(bot.send())
